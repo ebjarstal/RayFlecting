55762d1de5531966c1ebdbc5ac060a95af27ec93
fojd <ericbmousy@gmail.com>
2025-02-09 16:10:28 +0100

    Forgot to add rays.h to previous commit

--- rays.h
+++ rays.h
@@ -19,6 +19,8 @@ private:
 		const SDL_Point p1_start, SDL_Point* p2_end, double* r_a, SDL_Point* p1_prime,
 		const SDL_Rect* obstacles, const int count) const;
 public:
+	Ray() : origin({0, 0}), p1({0, 0}), p2({0, 0}), angle(0), r(0), g(0), b(0), a(0), nb_reflections(0) {}
+
 	Ray(SDL_Point o, SDL_Point p, int n) :
 		origin(o), p1(p), p2({0, 0}),
 		angle(std::atan2(o.y - p.y, p.x - o.x)),
@@ -36,5 +38,5 @@ public:
 	void SetColor(const Uint8 new_r, const Uint8 new_g, const Uint8 new_b, const Uint8 new_a);
 	void Draw(SDL_Renderer* renderer, const SDL_Rect* obstacles, const int count);
 	void DrawReflections(SDL_Renderer* renderer, const SDL_Rect* obstacles, const int count);
-	void AddReflection(const int n) { if (nb_reflections > 1) nb_reflections += n; }
+	void AddReflection(const int n);
 };
